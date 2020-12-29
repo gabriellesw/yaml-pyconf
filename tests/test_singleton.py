d5f7a888f0f427bd9a20d58310e679c43b1fd123
Gabrielle Sloane Law <rockymcrockerson@gabrielsmacbook.lan>
2020-12-29 14:10:26 -0800

    first commit

---
+++ tests/test_singleton.py
@@ -0,0 +1,10 @@
+import unittest
+from yaml_pyconf import SimpleConfig, FlaskConfig
+
+
+class TestIsSingleton(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(SimpleConfig(), SimpleConfig())
+
+    def test_flask(self):
+        self.assertEqual(FlaskConfig(), FlaskConfig())
